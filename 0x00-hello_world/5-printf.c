6c8011919756ad7de96a50c39d3b1323a3a7ef8c
DT <dtvangogh@github.com>
2020-02-05 13:27:53 -0500

    fix betty format with **

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**                                                                                                      
+ * main - Main function                                                                                  
+ *                                                                                                       
+ * Return: 0 on success                                                                                  
+ */
 int main()
 {
         printf("with proper grammar, but the outcome is a piece of art,\n");
