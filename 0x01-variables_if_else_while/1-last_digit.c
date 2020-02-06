0db8b61719253376d1453f0583c6902203e2fc7c
dt <dtvangogh@github.com>
2020-02-06 19:21:41 +0000

    completing the problem with two variables (last and n) in order to save typing and collect style points

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main-the main function
+*
+*return-Always 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int last;
+last = n % 10;
+printf("Last digit of %d is ", n);
+if (last > 5)
+printf("%d and is greater than 5\n", last);
+else if ((n % 10) == 0)
+printf("%d and is 0\n", last);
+else
+printf("%d and is less than 6 and not 0\n", last);
+return (0);
+}
