dc527045827a8e1c5ed268f477b3a0816dbefb62
dt <dtvangogh@github.com>
2020-02-06 18:18:55 +0000

    fix betty formats

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+*main-main function
+*
+*return-Always 0
+*/
+
 int main(void)
 {
 int n;
