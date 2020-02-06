1870ae1ddd6145f42e77c0588304d494bd13aa28
dt <dtvangogh@github.com>
2020-02-06 20:06:24 +0000

    use char c, set parameters a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Main function
+*
+*Return: 0 on Success
+*/
+
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+printf("%c ", c);
+return (0);
+}
