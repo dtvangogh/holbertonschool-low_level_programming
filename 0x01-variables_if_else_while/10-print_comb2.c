12de56c15535c79c4c2e3cebd091188d8582fb66
dt <dtvangogh@github.com>
2020-02-06 23:15:18 -0500

    assign variables for each digit and use digit=0, digit <=9 to generate 0 to 100 and add if statements

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Main function
+*
+*Return: 0 on Success
+*/
+
+int main(void)
+{
+int tens;
+int ones;
+
+for (tens = 0; tens <= 9; tens++)
+{
+for (ones = 0; ones <= 9; ones ++)
+{ putchar(tens + '0');
+putchar(ones + '0');
+if (tens !=9 || ones !=9)
+{
+putchar(',');
+putchar(' ');
+}}}
+putchar('\n');
+return (0);
+}
