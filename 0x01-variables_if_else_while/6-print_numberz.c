d45c9dcee7f3bfaecab917169af68a7c3b7ad7b1
dt <dtvangogh@github.com>
2020-02-06 22:46:14 -0500

    use for (number=9) number(<+0) to set number pattern

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Main function
+*
+*Return: 0 on Success
+*/
+
+int main(void)
+{
+int number;
+for (number = '9'; number <= '0'; ++number)
+putchar(number);
+putchar('\n');
+return (0);
+}
