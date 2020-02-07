5b1af4a799192816e8943311bdc8f5f3851fa73d
dt <dtvangogh@github.com>
2020-02-06 23:17:16 -0500

    assign variables for each digit and use digit=0, digit <=9 to generate 0 to 100 sand putchar the , comma and __space

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -15,10 +15,9 @@ int ones;
 
 for (tens = 0; tens <= 9; tens++)
 {
-for (ones = 0; ones <= 9; ones ++)
+for (ones = 0; ones <= 9; ones++)
 { putchar(tens + '0');
 putchar(ones + '0');
-if (tens !=9 || ones !=9)
 {
 putchar(',');
 putchar(' ');
