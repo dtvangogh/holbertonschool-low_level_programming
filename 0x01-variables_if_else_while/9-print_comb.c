326604e0c3d357770ecb8454503c0ac97719ae24
dt <dtvangogh@github.com>
2020-02-07 21:29:31 +0000

    use =0 <=9 to set perimeter for loop and if statement to end the loop at 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Main function
+*
+*Return: 0 on Success
+**/
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar(num + '0');
+if (num < 9)
+{
+putchar(',');
+putchar(' ');
+}}
+putchar('\n');
+return (0);
+}
