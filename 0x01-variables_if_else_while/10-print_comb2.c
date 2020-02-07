4e3969c9f0e694355275f74015535414f4bfd44e
dt <dtvangogh@github.com>
2020-02-06 23:35:55 -0500

    use if statement if number=99 to remove the comma

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -19,9 +19,13 @@ for (ones = 0; ones <= 9; ones++)
 { putchar(tens + '0');
 putchar(ones + '0');
 {
+if (tens == 9 && ones == 9)
+{}
+else
+{
 putchar(',');
 putchar(' ');
-}}}
+}}}}
 putchar('\n');
 return (0);
 }
