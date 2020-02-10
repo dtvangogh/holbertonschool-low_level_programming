409c26188b029ecfa2a08a0484f81e69e86c0532
dt <dtvangogh@github.com>
2020-02-10 16:10:22 -0500

    use ascii values to determine if c is a lowercase letter. if c is >= a or <= z than c is a lowercase letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - checks for lowercase letters
+ *
+ * Return: Always 0.
+ */
+int main(void)
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
