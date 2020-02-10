115e7e84f7b217ed9ad017ea33561c48799fca02
dt <dtvangogh@github.com>
2020-02-09 22:37:09 -0500

    use %d as a placeholder and use (int)sizeof(type) as variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,16 @@
-C
+#include <stdio.h>
+
+/**
+*main - Prints the size of different types
+*
+*Return: 0
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+	return (0);
+}
