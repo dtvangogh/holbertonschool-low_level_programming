f3777ee2d10b10b33366098ea5d247830ccf6b8e
dt <dtvangogh@github.com>
2020-02-11 11:54:19 -0500

    return (a+b) to finish the function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*add -adds two integers and returns result
+*
+*Return:0
+*
+*@a: value of integer
+*
+*@b: value of integer
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
