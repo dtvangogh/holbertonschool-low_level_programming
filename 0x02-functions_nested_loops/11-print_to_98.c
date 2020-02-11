653b9c033901e9292edf136f5763b15007236557
dt <dtvangogh@github.com>
2020-02-11 12:47:07 -0500

    if n<98 +1 to display all parameters and if n>98 -1 to display 111 to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_to_98 - print all natural numbers
+*
+*@n: integer value of number
+**/
+
+void print_to_98(int n)
+{
+if (n == 98)
+{
+printf("%d\n", n);
+}
+else if (n < 98)
+{
+printf("%d, ", n);
+print_to_98(n + 1);
+}
+else if (n > 98)
+{
+printf("%d, ", n);
+print_to_98(n - 1);
+}
+}
