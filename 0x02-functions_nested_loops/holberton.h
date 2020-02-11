c3cd58a9052be407583951acc5bb251086f7d74e
dt <dtvangogh@github.com>
2020-02-11 09:27:31 -0500

    custom header page for nested loop problems

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,9 +1,9 @@
-int add(int a, int b)
-{
-return(a+b);
-}
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n); 
 
-int _putchar(char c);
-
-
-
+void jack_bauer(void);
