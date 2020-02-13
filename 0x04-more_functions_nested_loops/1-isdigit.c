58a63df8e6304379652b4ff621ada6ae4841b8f3
dt <dtvangogh@github.com>
2020-02-13 13:59:43 -0500

    if c is a digit 0-9 return 1, else return 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*int _isdigit(int c)-finds if c is a digit
+*
+*Return: 1 if digit and 0 if else
+*
+*@c:a charactr
+**/
+
+int _isdigit(int c)
+{
+	if (c > '0' || c >= '9')
+	return (0);
+	else
+	return (1);
+}
