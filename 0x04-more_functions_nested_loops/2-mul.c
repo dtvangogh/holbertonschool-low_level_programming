403ede9e853f5da65b3ae81e9adf07e9c9151e63
dt <dtvangogh@github.com>
2020-02-13 14:21:43 -0500

    return a*b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*int mul(int a, int b);
+*
+*Description: multplies two integers
+*
+*Return: Always 0
+*
+*@a: one intenger
+*
+*@b: second integer
+**/
+
+int mul(int a, int b)
+	{
+	return (a * b);
+	}
