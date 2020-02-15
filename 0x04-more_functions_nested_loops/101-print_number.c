f635d16d978ccf12621e2c1d6047d23e8872729e
dt <dtvangogh@github.com>
2020-02-15 10:10:57 -0500

    print full integer but take away last digit with /10 then putchar and return last digit with %10

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_number - A function that prints an integer
+ * @n: integer value to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
