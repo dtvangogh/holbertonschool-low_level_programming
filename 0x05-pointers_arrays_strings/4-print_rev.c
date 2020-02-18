16e6a28cd52f35ea7d6513e292645e1a3e54c130
dt <dtvangogh@github.com>
2020-02-18 14:45:14 -0500

    use a>=0; decrement to reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*print_rev - print a string in reverse
+*description - reverses a string
+*Return: 0
+*@s: a variable to represent a string
+**/
+
+void print_rev(char *s)
+{
+
+	int a;
+
+	while (s[a] != '\0')
+	a++;
+
+	for (; a >= 0; a--)
+		_putchar(s[a]);
+	_putchar('\n');
+}
