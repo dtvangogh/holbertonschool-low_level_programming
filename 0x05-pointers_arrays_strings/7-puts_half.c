265d1da4a1cc6853d5ba919d857d074846a9c152
dt <dtvangogh@github.com>
2020-02-18 16:41:41 -0500

    use stringlength/2 if even stringlength and if not -1 /2

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+void puts_half(char *str)
+{
+
+int a; 
+
+int stringlength = _strlen(str);
+
+	if (stringlength % 2 == 0)
+	{
+		for (a= stringlength  / 2; a < stringlength; a++)
+			_putchar(str[a]);
+	}
+	else
+	{
+		for (a = stringlength - (stringlength - 1) / 2; a < stringlength; a++)
+			_putchar(str[a]);
+	}
+	_putchar('\n');
+}
+
+/**
+*_strlen - finds the length of a string
+*description - returns length of string
+*Return: string length
+*@s: a string
+**/
+
+int _strlen(char *s)
+{
+
+	int stringlength;
+
+	stringlength = 0;
+
+	for (stringlength = 0; s[stringlength] != '\0'; ++stringlength)
+	{}
+
+
+	return (stringlength);
+
+}
