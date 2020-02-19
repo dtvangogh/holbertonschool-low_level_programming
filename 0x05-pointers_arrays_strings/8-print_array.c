2dbfa42635fa75d2e46496237566880f57945033
dt <dtvangogh@github.com>
2020-02-19 14:11:08 -0500

    use for loop, where start =0 up to n

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*print_array - prints an array
+*description - prints the output of a given array
+*@a: elements of the array
+*@n: number of elements of the array that will be printed
+*/
+
+void print_array(int *a, int n)
+{
+int start;
+
+	for (start = 0; start < n; start++)
+	{
+		printf("%d", a[start]);
+		if (start < n)
+		{
+		printf(", ");
+		}
+	}
+}
