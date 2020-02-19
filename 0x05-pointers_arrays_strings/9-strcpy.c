557d5c1850a422dce527e43a378b2a4a6c96c107
dt <dtvangogh@github.com>
2020-02-19 12:44:39 -0500

    use while loop to match every character from src to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/** 
+* *_strcpy - copies a string
+*Description: copies string from src into dest
+*@dest: destination variable
+*@src: the source
+**/
+
+char *_strcpy(char *dest, char *src) 
+{
+int start;
+
+start = 0;
+
+	while (src[start] !=0)
+	{
+		dest[start] = src [start];
+		start++;
+	}
+	dest[start] = src[start];
+	return (dest);
+}
+
+
