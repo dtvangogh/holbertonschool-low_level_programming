61b7e1672eb1e87bfe0f317f22ea62aab8cdabfc
dt <dtvangogh@github.com>
2020-02-19 21:54:04 -0500

    use print_number for numbers over 0 and putchar last digit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,20 @@
 #include "holberton.h"
 
 /**
- * print_number - A function that prints an integer
+ * print_number - A function that prints any integer up to 1,000,000,000
  * @n: integer value to print
  */
 void print_number(int n)
 {
-	if (n < 0)
+	if (n < 0)   /* prints negative numbers */
 	{
 		n = -n;
 		_putchar('-');
 	}
+	if (n < 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
 
-	if (n < 100 && n > 0)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	if (n < 1000 && n > 99)
-	{
-		_putchar((n / 100) + '0');
-		_putchar((n / 10 % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	if (n < 10000 && n > 999)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar((n / 100 % 10) + '0');
-		_putchar((n / 10 % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	if (n == 0)
-		_putchar(n + '0');
 }
