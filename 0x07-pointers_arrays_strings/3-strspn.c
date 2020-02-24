760ca2251e1d84e84d5b3b0e5c75352f44cdda29
dt <dtvangogh@github.com>
2020-02-24 14:16:17 -0500

    increment s at the point where all characters match with accept string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+*_strspn - write function that gets length of prefix substring
+*@s: string
+*@accept: substring
+*Return: number of bytes of s that contains only bytes from accept
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int hello = 0;
+
+unsigned int oleh;
+
+	while (s[hello] != '\0')
+	{
+		oleh = 0;
+		while (accept[oleh] != '\0' && s[hello] != accept[oleh])
+			oleh++;
+	if (accept[oleh] == '\0')
+		return (hello);
+	hello++;
+	}
+
+	return (hello);
+}
+
+
