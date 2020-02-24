c5d6b8f0c1c412abab86bc4a23473657f140f9fd
dt <dtvangogh@github.com>
2020-02-24 13:52:21 -0500

    use *s==c to start at the point where c is found

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*_strchr - locates a character in a string
+*@s: the string
+*@c: the character the function will look for.
+*Return: 0
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+	if (*s == c)
+	return (s);
+	s++;
+	}
+
+	else
+		return (NULL);
+
+
+}
