70963da65af664931607b83f12837d5c6ea9aa3b
dt <dtvangogh@github.com>
2020-02-24 14:36:12 -0500

    change **s to *s and *to to to

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+*set _string - sets value of a pointer to a char
+*@s: double pointer
+*@to: another variable
+*
+*
+void set_string(char **s, char *to)
+{
+	*s = to;
+
+} 
