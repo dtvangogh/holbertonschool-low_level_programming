5c99f72327825a4175ba8681d19ed2fe4f578e66
dt <dtvangogh@github.com>
2020-02-24 14:39:40 -0500

    add description for function set_string

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 
 /**
-*set _string - sets value of a pointer to a char
+*set_string - sets value of a pointer to a char
+*description: turns palacios into anne cognet
 *@s: double pointer
 *@to: another variable
 *
-*
+*/
 void set_string(char **s, char *to)
 {
 	*s = to;
 
-} 
+}
