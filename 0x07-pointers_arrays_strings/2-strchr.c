4ae65a3f1f344e2382ba00e1c5ddd0049aa4bb14
dt <dtvangogh@github.com>
2020-02-24 13:56:11 -0500

    add another if statement outside of the while loop

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,12 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-	if (*s == c)
-	return (s);
-	s++;
+		if (*s == c)
+		return (s);
+		s++;
 	}
-
+	if (*s == c)
+		return (s);
 	else
 		return (NULL);
 
