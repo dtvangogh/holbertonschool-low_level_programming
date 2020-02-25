c674e857d304c3bedc974235288c06330e8b84c0
dt <dtvangogh@github.com>
2020-02-25 17:56:47 -0500

    use size + 1, size -1 to increment by diagonals and set limit of for loops to size * size to loop through whole array

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,9 +10,9 @@ int i; int j; int diagonal1 = 0; int diagonal2 = 0;
 	for (i = 0; i < size * size; i += size + 1)
 	diagonal1 += a[i]; 
 
-	for (j = size - 1; j < size * size; j += (size - 1))
+	for (j = size - 1; j < size * size - 1; j += (size - 1))
 
-	diagonal2 += a[i]; 
+	diagonal2 += a[j]; 
 
 
 	printf("%i, %i\n", diagonal1, diagonal2);
