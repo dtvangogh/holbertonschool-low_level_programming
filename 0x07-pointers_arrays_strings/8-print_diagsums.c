e557c0edaca08e5a62c08d754d83e37a44628b3b
dt <dtvangogh@github.com>
2020-02-25 18:01:21 -0500

    use size + 1, size -1 to increment by diagonals and set limit of for loops to size * size to loop through whole array

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,19 +1,27 @@
 #include "holberton.h"
 
 #include <stdio.h>
-
+/**
+*print_diagsums - prints the sum of two diagonals of a square matrix
+*description - takes a square matrix of integers and prints sum
+*@size: the size of the square matrix
+*@a: pointer to a square array of integers
+*/
 
 void print_diagsums(int *a, int size)
 {
-int i; int j; int diagonal1 = 0; int diagonal2 = 0; 
+int i;
+int j;
+int diagonal1 = 0;
+int diagonal2 = 0;
 
 	for (i = 0; i < size * size; i += size + 1)
-	diagonal1 += a[i]; 
+	diagonal1 += a[i];
 
 	for (j = size - 1; j < size * size - 1; j += (size - 1))
 
-	diagonal2 += a[j]; 
 
+	diagonal2 += a[j];
 
 	printf("%i, %i\n", diagonal1, diagonal2);
 }
