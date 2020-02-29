7bf282fefdb2b306b5f1356638572daa114214a0
dt <dtvangogh@github.com>
2020-02-29 09:42:55 -0500

    printf %s with variable argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main-prints name of program
+*@argc: size of argv
+*@argv: arguments passed
+*Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
