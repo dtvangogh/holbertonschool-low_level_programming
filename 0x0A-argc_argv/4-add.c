1cf97ace57390bdce54e3e235ad6cd9ee293c515
dt <dtvangogh@github.com>
2020-02-29 09:49:15 -0500

    use isdigit function to check for characters that arent numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main-calculates the sum of all positive numbers
+*@argc: size of argv
+*@argv: arguments passed to program
+*Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	int numbers;
+
+	int sum;
+
+	int i;
+
+	if (argc == 0)
+		printf("%d\n", 0);
+	while (argc > 0 && argc--)
+	{
+		i = 0;
+		while (argv[argc][i] != '\0')
+		{
+			i++;
+
+			if (!(isdigit(argv[argc][i] != 0)))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+		}
+		numbers = atoi(argv[argc]);
+		sum = sum + numbers;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
