f79305095b17094dad97d6acb40d161ff976e1dc
dt <dtvangogh@github.com>
2020-02-29 14:39:48 -0500

    alternative method

---
+++ 0x0A-argc_argv/4-alternative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main-calculates the sum of all positive numbers
+*@argc: size of argv
+*@argv: arguments passed to program
+*Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	int sum;
+
+	int i;
+
+	if (argc == 0)
+		printf("%d\n", 0);
+	while (argc-- && argc > 0)
+	{
+
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		
+		sum = sum + atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
