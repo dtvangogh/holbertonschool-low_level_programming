9f5a1747e267bd9c8942e0cb367427369037da81
dt <dtvangogh@github.com>
2020-03-03 13:20:01 -0500

    set size of arrow with malloc and then print array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h" 
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of chars
+ * @size: size of array
+ * @c: char c
+ * Return: c
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size != 0)
+		array = malloc(sizeof(c) * size);
+	else
+		return (NULL);
+i = 0; 
+	while (i < size)
+	{
+		array[i] = c;
+		i++;
+	}
+	return(array);
+} 
