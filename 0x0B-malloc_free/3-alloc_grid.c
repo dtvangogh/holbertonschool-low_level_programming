b636d0ea4675b0ac9c5a2b1f15e8f3c482becc2d
dt <dtvangogh@github.com>
2020-03-04 10:11:49 -0500

    mispelling of rows variable made program unable to compile

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 		grid[rows] = malloc(width * sizeof(int));
 		if (grid[rows] == NULL)
 		{
-			for (columns = 0; columns < row; columns++)
+			for (columns = 0; columns < rows; columns++)
 				free(grid[columns]);
 				free(grid);
 			return (NULL);
