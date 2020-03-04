e3eb9a4b082be530620d01588327ff555189a2ea
dt <dtvangogh@github.com>
2020-03-04 14:20:30 -0500

    remove redundant variable for indexdest

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+
 /**
  * argstostr - turns each argument into a string and display in separate line
  * Description: Concatenates all  arguments into one string
@@ -10,30 +11,30 @@
 
 char *argstostr(int ac, char **av)
 {
-	int sizedst, aindex, sindex, dstindex;
+	int sizedest, aindex, sindex;
 	char *dst;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (sizedst = 0, aindex = 0; aindex < ac; aindex++)
+	for (sizedest = 0, aindex = 0; aindex < ac; aindex++)
 		for (sindex = 0; av[aindex][sindex] != '\0'; sindex++)
-			sizedst++;
+			sizedest++;
 
-	dst = malloc(sizedst + ac + 1);
+	dst = malloc(sizedest + ac + 1);
 	if (dst == NULL)
 		return (NULL);
 
-	for (aindex = 0, dstindex = 0; aindex < ac; aindex++)
+	for (aindex = 0, sizedest = 0; aindex < ac; aindex++)
 	{
 		for (sindex = 0; av[aindex][sindex] != '\0'; sindex++)
 		{
-			dst[dstindex] = av[aindex][sindex];
-			dstindex++;
+			dst[sizedest] = av[aindex][sindex];
+			sizedest++;
 		}
-		dst[dstindex] = '\n';
-		dstindex++;
+		dst[sizedest] = '\n';
+		sizedest++;
 	}
-	dst[dstindex] = '\0';  /**add null bye**/
+	dst[sizedest] = '\0';  /**add null bye**/
 	return (dst);
 }
