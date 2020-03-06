a181e8e961dc34f8bb65dc8f034ecaa315697879
dt <dtvangogh@github.com>
2020-03-05 21:11:59 -0500

    fix increment min++, i++ by adding min + i within for loop

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - create anarray of integers (min to max)
+ * @min: min number to start
+ * @max: max number to start
+ * Return: pointer
+ */
+
+int *array_range(int min, int max)
+{
+	int *pointer;
+	int  i;
+	int difference;
+
+	difference = max - min;
+
+	if (min > max)
+		return (NULL);
+
+	pointer = malloc(sizeof(int) * (difference + 1));
+	if (pointer == NULL)
+		return (NULL);
+	for (i = 0; i <= difference; i++)
+	{
+		pointer[i] = min + i;
+	}
+	return (pointer);
+}
