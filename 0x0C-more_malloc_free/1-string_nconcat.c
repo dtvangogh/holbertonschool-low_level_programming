450a2aff2604a67a58d65cb034e782e147e93ed5
dt <dtvangogh@github.com>
2020-03-05 20:23:50 -0500

    while (i < n) to get length of s2 for concatenation

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1:stirng 1
+ * @s2:string 2
+ * @n: n bytesofstring 2
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *pointer;
+	unsigned int lengths1, lengths2, i;
+
+	if (s1 != NULL)
+		for (lengths1 = 0; s1[lengths1] != 0; lengths1++)
+		{}
+	if (s2 != NULL)
+		for (lengths2 = 0; s2[lengths2] != 0; lengths2++)
+		{}
+	if (n > lengths2)
+		lengths2 = n;
+	pointer = malloc(sizeof(char) * (lengths1 + n) + 1);
+		if (pointer == NULL)
+		return (NULL);
+
+		i = 0;
+	while (i < lengths1)
+	{
+		pointer[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		pointer[i + lengths1] = s2[i];
+		i++;
+	}
+	return (pointer);
+
+}
