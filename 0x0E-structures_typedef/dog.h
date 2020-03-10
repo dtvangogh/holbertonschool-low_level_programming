74de2fe7aa2c51032a8c9e4ceb5c296036438995
dt <dtvangogh@github.com>
2020-03-09 23:03:42 -0400

    fix order

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,7 +4,7 @@
  * struct dog- struture of a dog
  * @name: name of dog
  * @age: age of dog
- * @owner: owner of dog
+ * @owner: who is owner
  */
 struct dog
 {
@@ -18,7 +18,7 @@ struct dog
  */
 typedef struct dog dog_t;
 void free_dog(dog_t *d);
-void print_dog(struct dog *d)
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
