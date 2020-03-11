d455046df929438ebfb8891818b26f872033c96b
dt <dtvangogh@github.com>
2020-03-11 17:06:10 -0400

    main file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+  * main - Main
+  * @argc: count of arguments
+  * @argv: pointer to array
+  * Return: 0 on success, 98 on incorrect arguments, 99 if no operator
+  */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (get_op_func(argv[2]) == NULL)
+		printf("Error\n"), exit(99);
+	if (b == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+		printf("Error\n"), exit(100);
+	printf("%d\n", (get_op_func(argv[2])(a, b)));
+	return (0);
+}
