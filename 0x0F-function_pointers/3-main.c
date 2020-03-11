0b3c000d14a3a20b8f28907f00f942d22249f917
dt <dtvangogh@github.com>
2020-03-11 18:09:54 -0400

    remove test experiment for x

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	b = atoi(argv[3]);
 
 	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
-	    && argv[2][0] != '/'  && argv[2][0] != '%' && argv[2][0] != 'x') || get_op_func(argv[2]) == NULL)
+	    && argv[2][0] != '/'  && argv[2][0] != '%') || get_op_func(argv[2]) == NULL)
 		printf("Error\n"), exit(99);
 	if (b == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 		printf("Error\n"), exit(100);
