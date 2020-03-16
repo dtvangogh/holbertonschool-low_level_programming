3248f6d00f8fb5d3b858416e30dcc11327481b7c
dt <dtvangogh@github.com>
2020-03-15 22:29:40 -0400

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,9 +6,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-typedef struct fm
-{
-	char *fm;
-	void (*p)();
-} fm_t;
 #endif
