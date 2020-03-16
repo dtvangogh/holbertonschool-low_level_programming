7387c3cb295999cfe91bec1b164bf371814bb59e
dt <dtvangogh@github.com>
2020-03-15 22:32:42 -0400

    do not print separator for last number, or N

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ *  print_numbers - prints out numbers
+ * @separator: string printed out between the numbers
+ * @n: number of arguments
+ * Return: 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list numbers;
+	unsigned int i;
+
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(numbers, unsigned int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(numbers);
+	printf("\n");
+}
