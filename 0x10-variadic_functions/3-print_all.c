5e05bcaa16698ee246397591c48166d8fdc74b8c
dt <dtvangogh@github.com>
2020-03-15 22:44:26 -0400

    return void

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+*print_all - prints all
+*Return: 0
+*@format: list of types of args passed to f'n
+*/
+
+void print_all(const char* const format, ...)
+{
+
+}
