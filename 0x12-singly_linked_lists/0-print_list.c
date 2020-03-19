785a2bbddf4ec4d56a6390e0c79c02343c2599d2
dt <dtvangogh@github.com>
2020-03-19 15:19:30 -0400

    use if statement in order to use else statement to print -, nil

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,13 +8,11 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
-
-	while (h != NULL)
-	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		i++;
-	}
-	return (i);
+	while (h == NULL)
+		return (0);
+	if (h->str != NULL)
+		printf("[%i] %s\n", h->len, h->str);
+	else
+		printf("[0] (nil)\n");
+	return (1 +  print_list(h->next));
 }
