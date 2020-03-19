f48dbf283aa1b52becd0b6c7595d406258197e74
dt <dtvangogh@github.com>
2020-03-19 16:25:18 -0400

    in order to change list_t new must make make different string and copy from there

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/*
+*add_node - add a new node at the beginning
+*Description - adding new node shifts everything down
+*@str: the string added
+@head: the first string in list
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	int i, len;
+	char *copy;
+	list_t *new;
+
+	len = strlen(str);
+	new = *head;
+	copy = malloc(sizeof(char) * len + 1);
+	if (head == NULL || str == NULL || copy == NULL)
+		return (0);
+	for (i = 0; str[i]; i++)
+		copy[i] = str[i];
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	/*create copy string because list_t cannot be played with*/
+	new->len = len; /*iterate thru length first */
+	new->str = copy;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
