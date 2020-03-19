7e0b5de189933a9d01b27ba488df1c839829aa1a
dt <dtvangogh@github.com>
2020-03-19 16:30:35 -0400

    remove null statements

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,6 +7,7 @@
 *Description - adding new node shifts everything down
 *@str: the string added
 @head: the first string in list
+Return: new list
 */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -16,16 +17,14 @@ list_t *add_node(list_t **head, const char *str)
 
 	len = strlen(str);
 	new = *head;
-	copy = malloc(sizeof(char) * len + 1);
-	if (head == NULL || str == NULL || copy == NULL)
-		return (0);
-	for (i = 0; str[i]; i++)
-		copy[i] = str[i];
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
+	copy = malloc(sizeof(char) * len + 1);
+	for (i = 0; str[i]; i++)
+		copy[i] = str[i];
 	new->len = len; /*iterate thru length first */
 	new->str = copy;
 	new->next = *head;
