b33e4fc183dce31cb1e29c975a6c29a3c4a99850
dt <dtvangogh@github.com>
2020-03-19 18:38:55 -0400

    remove strlen function --illegal

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
-*add_node - add a new node at the beginning
-*Description - adding new node shifts everything down
+**add_node- adds new node at the beginning
+*Description- adding new node shifts everything down
 *@str: the string added
 @head: the first string in list
-Return: new list
+*Return: new list
 */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -15,7 +15,9 @@ list_t *add_node(list_t **head, const char *str)
 	char *copy;
 	list_t *new;
 
-	len = strlen(str);
+	len = 0;
+	while (str[len] != '\0')
+		len++;
 	new = *head;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
