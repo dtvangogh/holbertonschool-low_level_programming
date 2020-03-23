78218ebbf0697be259abe14ae7531365c76fdc1e
dt <dtvangogh@github.com>
2020-03-23 17:56:41 -0400

    print nil if str=null

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,6 +14,9 @@ size_t print_list(const list_t *h)
 	count = 0;
 	while (h !=  NULL)
 	{
+		if (h->str == NULL)
+		printf("[%d] %s\n", h->len, "(nil)");
+		else
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
