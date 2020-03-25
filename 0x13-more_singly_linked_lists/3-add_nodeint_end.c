5b2d6fd8359970a1648452f8874f68ecb98b94eb
dt <dtvangogh@github.com>
2020-03-24 22:32:07 -0400

    allocate memory for list with malloc

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint_end - add a new node at the end
+ * @head: pointer to the linked list
+ * @n: number data inside node
+ * Return: the address of the new element
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newnode;
+	listint_t *temp;
+
+	newnode = malloc(sizeof(listint_t));
+	newnode->n = n;
+	newnode->next = NULL;
+	if (*head == NULL)
+	{
+		*head = newnode;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = newnode;
+
+		if (newnode == NULL)
+			return (NULL);
+	}
+	return (newnode);
+}
