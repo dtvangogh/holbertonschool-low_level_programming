0a43590415f42127c35aa408e273172dd4b3cb95
dt <dtvangogh@github.com>
2020-03-24 22:33:20 -0400

    null conditional before new node is added

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,15 +21,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		*head = newnode;
 	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = newnode;
 
-		if (newnode == NULL)
-			return (NULL);
-	}
+	if (newnode == NULL)
+		return (NULL) else
+		{
+			temp = *head;
+			while (temp->next != NULL)
+				temp = temp->next;
+			temp->next = newnode;
+		}
 	return (newnode);
 }
