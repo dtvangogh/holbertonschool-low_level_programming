5e44baf145e084ce940ec1f6ffeda345400e969a
dt <dtvangogh@github.com>
2020-03-24 22:36:26 -0400

    place null conditional right after malloc

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,6 +15,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *temp;
 
 	newnode = malloc(sizeof(listint_t));
+
+	if (newnode == NULL)
+		return (NULL);
 	newnode->n = n;
 	newnode->next = NULL;
 	if (*head == NULL)
@@ -24,9 +27,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	else
 	{
-
-		if (newnode == NULL)
-			return (NULL);
 		temp = *head;
 		while (temp->next != NULL)
 			temp = temp->next;
