1f2e9b5dbf85342394a6e09199cdebd18ddbf7af
dt <dtvangogh@github.com>
2020-05-03 14:14:25 -0400

    sum

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,22 @@
-This is my first line of code
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of data in a double linked list
+ * @head: head
+ * Return: sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+	dlistint_t *temporary;
+
+	temporary = head;
+	sum = 0;
+	while (temporary != NULL)
+	{
+		sum += temporary->n;
+		temporary = temporary->next;
+	}
+	return (sum);
+}
