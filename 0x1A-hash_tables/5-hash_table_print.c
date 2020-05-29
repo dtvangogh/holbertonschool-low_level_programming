12f7e5ecbfe3d499b1b64ccef3d24ef75282d9f9
dt <dtvangogh@github.com>
2020-05-29 17:14:09 -0400

    hash

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ *hash_table_print - prints a hash table
+ *@ht: the hash table
+ *
+ *Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	while (index < ht->size)
+	{
+		node = ht->array[index];
+		while (node != NULL)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+		}
+		index++;
+	}
+	printf("}\n");
+}
