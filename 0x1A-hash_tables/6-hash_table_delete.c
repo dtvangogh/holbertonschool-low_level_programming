d727fa68506aebeab81ff3400e68fc7c59131cf7
dt <dtvangogh@github.com>
2020-05-29 18:12:06 -0400

    push

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,9 +10,6 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *temporary;
 	unsigned long int i = 0;
 
-	if (ht == NULL)
-		return (NULL);
-
 	while (i < ht->size)
 	{
 		while (ht->array[i] != NULL)
