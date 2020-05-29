950aa5bb92ae7d7f4372f909c38a925434ea71be
dt <dtvangogh@github.com>
2020-05-29 17:00:23 -0400

    return values

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ *hash_table_get - gets value of a key
+ *@ht: the hash
+ *@key: the key
+ *
+ *Return: value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index_of_key;
+	hash_node_t *has_node;
+
+	index_of_key = key_index((unsigned char *)key, ht->size);
+	hash_node = ht->array[index_of_key];
+	if (hash_node == NULL)
+		return (NULL);
+	/*Check if there is a linked list entry*/
+	while (hash_node != NULL)
+	{
+		if (strcmp(hash_node->key, key) == 0)
+			return (hash_node->value);
+		hash_node = hash_node->next;
+	}
+	return (NULL);
+}
